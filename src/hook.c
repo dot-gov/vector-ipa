4d26c3b30b3321628033e7400052aca8fe8acbb7
alor <alberto.ornaghi@gmail.com>
2011-07-28 10:22:52 +0200

    rw lock on hooks

--- src/hook.c
+++ src/hook.c
@@ -22,6 +22,8 @@ struct hook_list {
 /* the list for the HOOK_* */
 static LIST_HEAD(, hook_list) hook_list_head[HOOK_PACKET_MAX];
 
+pthread_rwlock_t hook_lock;
+
 /* protos... */
 
 void hook_point(int point, struct packet_object *po);
@@ -30,14 +32,21 @@ int hook_del(int point, void (*func)(struct packet_object *po) );
 
 /*******************************************/
 
+void __init hook_init(void)
+{
+   pthread_rwlock_init(&hook_lock);
+}
+
 /* execute the functions registered in that hook point */
 
 void hook_point(int point, struct packet_object *po)
 {
    struct hook_list *current;
 
+   pthread_rwlock_rdlock(&hook_lock);
    LIST_FOREACH(current, &hook_list_head[point], next)
       current->func(po);
+   pthread_rwlock_unlock(&hook_lock);
 
    return;
 }
@@ -53,8 +62,11 @@ void hook_add(int point, void (*func)(struct packet_object *po) )
 
    newelem->func = func;
 
+   pthread_rwlock_wrlock(&hook_lock);
    LIST_INSERT_HEAD(&hook_list_head[point], newelem, next);
+   pthread_rwlock_unlock(&hook_lock);
 }
+
 
 /* remove a function from an hook point */
 
@@ -62,14 +74,17 @@ int hook_del(int point, void (*func)(struct packet_object *po) )
 {
    struct hook_list *current;
 
+   pthread_rwlock_wrlock(&hook_lock);
    LIST_FOREACH(current, &hook_list_head[point], next) {
       if (current->func == func) {
          LIST_REMOVE(current, next);
          SAFE_FREE(current);
          DEBUG_MSG(D_DEBUG, "hook_del -- %d [%p]", point, func);
+         pthread_rwlock_unlock(&hook_lock);
          return ESUCCESS;
       }
    }
+   pthread_rwlock_unlock(&hook_lock);
 
    return -ENOTFOUND;
 }
