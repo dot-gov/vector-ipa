df9b54ba59889d9a0fe507f9aaf2dd5eaadd7b1b
spikey <a.dipasquale@hackingteam.it>
2013-02-28 10:50:33 +0100

    Correct version for 8.3.0 release

--- include/version.h
+++ include/version.h
@@ -4,9 +4,9 @@
 #ifndef __VERS_H
 #define __VERS_H
 
-#define RCS_VER            "2012102901"
-#define VERSION            "2012.10.29"
-#define COPYRIGHT          "2008-2012"
+#define RCS_VER            "2013031101"
+#define VERSION            "2013.03.11"
+#define COPYRIGHT          "2008-2013"
 #define AUTHORS            "HackingTeam srl"
 
 #endif
