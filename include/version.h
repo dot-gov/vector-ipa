8b88812dcd6b4ebf5842894c65cc55cc98cc2f46
spikey <a.dipasquale@hackingteam.it>
2013-10-07 10:13:34 +0200

    Correct version to 9.0.0 Galileo

--- include/version.h
+++ include/version.h
@@ -4,8 +4,8 @@
 #ifndef __VERS_H
 #define __VERS_H
 
-#define RCS_VER            "2013070801"
-#define VERSION            "2013.07.08"
+#define RCS_VER            "2013103101"
+#define VERSION            "2013.10.31"
 #define COPYRIGHT          "2008-2013"
 #define AUTHORS            "HackingTeam srl"
 
